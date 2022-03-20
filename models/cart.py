82e08240f3dc421f8b4c592a86d6f02740209ea3
cicerotcv <elm.tiago@gmail.com>
2022-03-20 19:35:01 -0300

    CRUD ok

--- models/cart.py
+++ models/cart.py
@@ -1,20 +1,21 @@
 from pydantic import BaseModel
 
+
 class Cart(BaseModel):
     cart_id: int
     user_id: int
 
+
 class CartCreation(BaseModel):
     user_id: int
-    
-class CartNoID(BaseModel):
-    user_id: int
+
 
 class CartProduct(BaseModel):
     cart_id: int
     product_name: str
     quantity: int
 
-class CartProductNoID(BaseModel):
-    product_name: str
+
+class CartProductUpdate(BaseModel):
     quantity: int
+    product_name: str
